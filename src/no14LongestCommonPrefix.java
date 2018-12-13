45dbdd448602808641f3f7c9c21968bf62bcda5d
lucius <87913305@qq.com>
2018-12-13 17:04:43 +0800

    Initial commit

---
+++ src/no14LongestCommonPrefix.java
@@ -0,0 +1,32 @@
+public class no14LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuffer results = new StringBuffer();
+        if (strs != null && strs.length > 0 ) {
+            //取最小长度
+            int length = strs[0].length();
+            for (String st : strs) {
+                if (st.length() <= length) {
+                    length = st.length();
+                }
+            }
+            //寻找公共前缀
+            String temp = "";
+            boolean exits = false;
+            for (int i=0 ; i < length ; i++) {
+                temp = strs[0].substring(i,i+1);
+                for (String st : strs) {
+                    if (!temp.equals(st.substring(i,i+1))) {
+                        exits = true;
+                        break;
+                    }
+                }
+                if (exits) {
+                    break;
+                } else {
+                    results.append(temp);
+                }
+            }
+        }
+        return results.toString();
+    }
+}
---
+++ src/no7ReverseInteger.java
@@ -0,0 +1,28 @@
+public class no7ReverseInteger {
+
+    public static void main(String[] args) {
+        int x = -123;
+        String xStr = String.valueOf(x);
+        StringBuffer xStrB = new StringBuffer(xStr);
+        xStrB.reverse();
+        if (x < 0) {
+            if (xStr != null && !xStr.isEmpty()) {
+                xStrB.deleteCharAt(xStr.length()-1);
+            }
+            try {
+               x = -Integer.valueOf(xStrB.toString());
+            } catch (Exception e) {
+
+            }
+        } else {
+            try {
+                x = Integer.valueOf(xStrB.toString());
+            } catch (Exception e) {
+
+            }
+        }
+
+
+        System.out.println("goono:"+x);
+    }
+}
