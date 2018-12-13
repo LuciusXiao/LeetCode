9e9b4af074018459e12fb1da790131779437d8a5
lucius <87913305@qq.com>
2018-12-13 17:08:46 +0800

    add No.28 实现strStr()

---
+++ src/no28ImplementStrstr.java
@@ -0,0 +1,24 @@
+public class no28ImplementStrstr {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        if (haystack.equals(needle)) {
+            return 0;
+        }
+        if (haystack.length() == 0  || haystack.length() < needle.length()) {
+            return -1;
+        }
+        CharSequence needles = needle;
+        if (!haystack.contains(needles)) {
+            return -1;
+        }
+        for (int i = 0; i < haystack.length()-needle.length()+1 ; i++) {
+            if (haystack.substring(i,i+needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
